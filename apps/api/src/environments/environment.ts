9f2686462f03599be845c0a44a4a66a754689ad5
Dallas Cook <dallascook@Dallass-MacBook-Pro.local>
2019-10-01 11:25:04 -0600

    DB password for postgres user.

--- apps/api/src/environments/environment.ts
+++ apps/api/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   DATABASE: 'postgres',
   DATABASE_USER: 'postgres',
-  DATABASE_PASSWORD: '',
+  DATABASE_PASSWORD: 'P@ssw0rd1',
   DATABASE_HOST: '159.89.145.9'
 };
